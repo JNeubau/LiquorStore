43dd19e38084a079d05954b5f5c5b59ffb6eb24d
Patrycja Przybylska <patr.przybylska@gmail.com>
2022-06-13 16:58:35 +0200

    Niedziałające futro

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -1,61 +1,53 @@
 #version 330
 
-uniform sampler2D textureMap0;
-uniform sampler2D textureMap1;
-uniform sampler2D textureMap2;
+out vec4 pixelColor; //Output variable. Almost final pixel color.
 
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
-
-//in vec4 ic; 
-//in vec4 n;
+//Varying variables
+in vec4 ic;
+in vec4 n;
 in vec4 l;
 in vec4 v;
-in vec2 iTexCoord0;
-
-
-vec2 parallaxTexCoords(vec4 v, vec2 t, float h, float s){
-	vec2 ti = -v.xy/s;	// oblicz przyrosty
-	float hi = -v.z/s;	// przyrost wysokości
-
-	//zmienne robocze i aktualna wysokość
-	vec2 tc = t;
-	float hc = h;
-
-	float ht = h*texture(textureMap2, tc).r;
-
-	if(v.z <= 0) discard;
-	while(hc>ht) {
-		tc = tc+ti;
-		if(tc.x<0 || tc.x>1 || tc.y<0 || tc.y>1) discard;
-		hc = hc+hi;	// krok w głąb
-		ht = h*texture(textureMap2, tc).r;
-	}
-	return tc;
-}
+in vec4 n1;
+in vec4 l1;
+in vec4 v1;
+uniform sampler2D textureMap0; //globalnie
+uniform sampler2D textureMap1; //globalnie
+in vec2 iTexCoord0; //globalnie
+in vec2 iTexCoord1;
 
 
 void main(void) {
-
-	vec4 mv = normalize(v);
-	vec2 nt = parallaxTexCoords(mv, iTexCoord0, 0.1, 100);
-
-	//Znormalizowane interpolowane wektory
+	//Normalized, interpolated vectors
 	vec4 ml = normalize(l);
-	// vec4 mn = normalize(n);
-	//vec4 mn = normalize(vec4(texture(textureMap1, iTexCoord0).rgb*2-1, 0));
-	vec4 mn = normalize(vec4(texture(textureMap1, nt).rgb*2-1, 0));
-	
-	//Wektor odbity
+	vec4 mn = normalize(n);
+	vec4 mv = normalize(v);
+	//Reflected vector
 	vec4 mr = reflect(-ml, mn);
 
-	//Parametry powierzchni
-	//vec4 kd = texture(textureMap0, iTexCoord0); 
-	vec4 kd = texture(textureMap0, nt); 
-	// vec4 ks = texture(textureMap1, iTexCoord0);
-	vec4 ks = vec4(1, 1, 1, 1);
+	vec4 ml1 = normalize(l1);
+	vec4 mn1 = normalize(n1);
+	vec4 mv1 = normalize(v1);
+	//Reflected vector
+	vec4 mr1 = reflect(-ml1, mn1);
 
-	//Obliczenie modelu oświetlenia
+	//Surface parameters
+	//vec4 kd = ic;
+	vec4 texColor0=texture(textureMap0,iTexCoord0);//main
+	//vec4 texColor1=texture(textureMap1,iTexCoord1);//main
+	vec4 kd = ((3*texColor0)/5);
+	kd.a = ic.a;
+	//vec4 ks = texture(textureMap1, iTexCoord0);
+	vec4 ks= vec4(1,1,1,1);
+
+	//Lighting model computation
 	float nl = clamp(dot(mn, ml), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1),25);
-	pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+	float rv = pow(clamp(dot(mr, mv), 0, 1), 50);
+
+	float nl1 = clamp(dot(mn1, ml1), 0, 1);
+	float rv1 = pow(clamp(dot(mr1, mv1), 0, 1), 50);
+
+	//working 2 sources of light
+	pixelColor = vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb * rv, 0) + vec4(kd.rgb * nl1, 0) + vec4(ks.rgb * rv1, 0);
+	//pixelColor = vec4(kd.rgb * nl, 0.1) + vec4(ks.rgb * rv, 0);
+
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -1,40 +1,43 @@
 #version 330
 
-//Zmienne jednorodne
+//Uniform variables
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-//Atrybuty
-in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-//in vec4 color; //kolor związany z wierzchołkiem
-//in vec4 normal; //wektor normalny w przestrzeni modelu
+//Attributes
+in vec4 vertex; //Vertex coordinates in model space
+in vec4 color; //vertex color
+in vec4 normal; //Vertex normal in model space
 in vec2 texCoord0;
+out vec2 iTexCoord0; //globalnie
+out vec2 iTexCoord1; //globalnie
 
-in vec4 c1;
-in vec4 c2;
-in vec4 c3;
-
-//Zmienne interpolowane
-//out vec4 ic;
+//Varying variables
+out vec4 ic;
 out vec4 l;
-//out vec4 n;
+out vec4 n;
 out vec4 v;
-out vec2 iTexCoord0;
-
+out vec4 l1;
+out vec4 n1;
+out vec4 v1;
 
 void main(void) {
-	mat4 invTBN = mat4(c1, c2, c3, vec4(0,0,0,1));
+    vec4 lp = vec4(0, 0, 5, 0.02); //light position, world space
+    l = normalize(V * lp - V * M * vertex); //vector towards the light in eye space
+    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //vector towards the viewer in eye space
+    n = normalize(V * M * normal); //normal vector in eye space
 
-    vec4 lp = vec4(0, 0, -6, 1); //przestrzeń świata
-    // l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
-	l = normalize(invTBN*(inverse(M)*lp - vertex)); //wektor do światła w przestrzeni oka
-    // v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
-	v = normalize(invTBN*(inverse(V * M)*vec4(0,0,0,1) - vertex)); //wektor do obserwatora w przestrzeni oka
-    //n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
-    iTexCoord0 = texCoord0;
+    vec4 lp1 = vec4(0, 0, -5, 0.02); //light position, world space
+    l1 = normalize(V * lp1 - V * M * vertex); //vector towards the light in eye space
+    v1 = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //vector towards the viewer in eye space
+    n1 = normalize(V * M * normal); //normal vector in eye space
+	iTexCoord0=texCoord0; //w main
+    ic = color;
 
-    //ic = color;
+	vec4 eyeN=normalize(V*M*normal);
+	iTexCoord1=(eyeN.xy+1)/2;
     
-    gl_Position=P*V*M*vertex;
+	//working 2 sources of light
+    gl_Position = P * V * M * vertex;
 }
